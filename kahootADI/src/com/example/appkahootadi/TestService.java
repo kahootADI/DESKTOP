2c811bb402b577c0a2025681f80c4be516c9de4f
Naxoht <73888159+Naxoht@users.noreply.github.com>
2021-11-24 01:10:51 +0100

    New functions in server

--- kahootADI/src/com/example/appkahootadi/TestService.java
+++ kahootADI/src/com/example/appkahootadi/TestService.java
@@ -4,8 +4,7 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface TestService{
-
 	  public String getNickname(String nickname);
 	  public boolean kahootStarted();
-
+	  public boolean checkNicknames(String actualNickname);
 	}
